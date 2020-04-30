7716dce64d3d08964d5823d31713c1ac5880a506
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-30 20:03:53 +0300

    Add DriverBinary

---
+++ WebDriversGenerator/DriverBinary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriversGenerator
+{
+    public class DriverBinary
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Platform { get; set; }
+        public string Link { get; set; }
+    }
+}
